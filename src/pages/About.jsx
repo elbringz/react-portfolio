357dc992cbd2df0c54da12f26b6bc5854e805c76
Zachary Elbring <elbringzach@gmail.com>
2024-03-11 14:20:53 -0500

    Created about me page and added photo to assets.

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return(
+        <div id="about">
+            <h2>About Me</h2>
+            <p>Thanks for checking out my portfolio! I am currently a coding bootcamp student. I've been interested in tech my whole life and enjoy building custom PCs. Outside of class I work in a restaurant. I also enjoy fishing, hiking, and playing video games. In the 'My Projects' section you will find screenshots of my projects which are linked to the deployed product. In the 'Contact Info' section you will find a link to my Github and my email address.
+            </p>
+            <img src="../assets/tunnel-hike.jpg"/>
+        </div>
+    )
+}
